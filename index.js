b87fa337ab711aed26a307a0dcab5b427c1f497f
Holly T <144973465+thompsonholly@users.noreply.github.com>
2024-04-27 15:28:36 -0500

    if statements

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let num1 = 1;
-let num2 = 10;
-let firstCard = num1;
-let secondCard = num2;
-let sum = firstCard + secondCard;
+
+
+
+// let hitCard = num3;
+let cards = []
+let sum = 0
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 // getting message paragraph by id
 let messageEl = document.getElementById("message-el")
@@ -13,9 +14,37 @@ let sumEl = document.getElementById("sum-el")
 // getting cards paragraph by id
 let cardsEl = document.getElementById("cards-el")
 
+//Creat a function, getRandomCard
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1 // 1-13
+  if (randomNumber > 10) {
+    return 10
+  } else if (randomNumber === 1) {
+    return 11
+  } else {
+    return randomNumber
+  }
+}
+
+
+
+// creating a startGame() function to call renderGame function to differeniate start and continue game
 function startGame() {
-  // replacing the cards in the DOM w/ "Cards: 1st and 2nd number"
-  cardsEl.textContent = "Cards: " + firstCard + secondCard
+  isAlive = true
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
+  renderGame()
+}
+
+function renderGame() {
+  // replacing the cards in the DOM w/ "Cards: 1st and 2nd number and hit card"
+  cardsEl.textContent = "Cards: "
+  // Create a for loop that renders out all the cards instead of just two
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " "
+  }
   // replacing the sum in the DOM 
   sumEl.textContent = "Sum:" + sum
   // console.log(sumEl)
@@ -29,6 +58,18 @@ function startGame() {
     isAlive = false
   }
   // replacing the "Want to play a round?" in the DOM
- messageEl.textContent = message
- 
+  messageEl.textContent = message
+
+}
+
+function newCard() { 
+  // Only allow the player to get a new card if she is IS alive and does NOT have Blackjack
+  let card = getRandomCard()
+  sum += card
+  // Push the card to the cards array
+  cards.push(card)
+
+
+  // sumEl.textContent = "Sum: " + sum + hitCard
+  renderGame()
 }
